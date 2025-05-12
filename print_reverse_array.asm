890eb9eb3a185ee7066894433f2986822605c23e
Luke Herbelin <lukeherbelin@ucsb.edu>
2025-05-12 16:16:57 -0700

    print reversed array works by demo

--- print_reverse_array.asm
+++ print_reverse_array.asm
@@ -6,12 +6,36 @@
 #   make all returned values from functions go in $v0
 
 .data
-	array: # Write the definition here
-	cout:  # Write the definition here
+	array: .word 1 2 3 4 5 6 7 8 9 10
+	cout:  .asciiz "The contents of the arrary are:\n"
+	newline:  .asciiz "\n"
+
 
 .text
 printArr:
     # TODO: Write your function code here
+	addi $a1, $a1, -1
+	move $t1, $a1
+	sll $t1, $t1, 2
+
+	move $t0, $a0
+	addu $t0, $t0, $t1
+	li $t2, 4
+	
+loop:
+	blt $a1, $zero, return
+	li $v0, 1
+	lw $a0, 0($t0)
+	syscall
+	li $v0, 4
+	la $a0, newline
+	syscall
+	subu $t0, $t0, $t2
+	addi $a1, $a1, -1
+	j loop
+
+return:
+	jr $ra
 
 main:  # DO NOT MODIFY THE MAIN SECTION
 	li $v0, 4
@@ -25,4 +49,6 @@ main:  # DO NOT MODIFY THE MAIN SECTION
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
+	li $v0, 10
+	syscall
 
