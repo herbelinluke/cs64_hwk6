57cc343a9b38add1b1fbed61846588aaaba84614
Luke Herbelin <lukeherbelin@ucsb.edu>
2025-05-12 14:48:32 -0700

    skeleton files

---
+++ conversion.asm
@@ -0,0 +1,23 @@
+# conversion.asm program
+# For CMPSC 64
+#
+# Don't forget to:
+#   make all arguments to any function go in $a0 and/or $a1
+#   make all returned values from functions go in $v0
+
+.text
+conv:
+    # TODO: Write your function code here
+
+main:  # DO NOT MODIFY THE MAIN SECTION
+    li $a0, 5
+    li $a1, 7
+
+    jal conv
+
+    move $a0, $v0
+    li $v0, 1
+    syscall
+
+exit:
+	# TODO: Write code to properly exit a SPIM simulation
---
+++ print_reverse_array.asm
@@ -0,0 +1,28 @@
+# print_array.asm program
+# For CMPSC 64
+#
+# Don't forget to:
+#   make all arguments to any function go in $a0 and/or $a1
+#   make all returned values from functions go in $v0
+
+.data
+	array: # Write the definition here
+	cout:  # Write the definition here
+
+.text
+printArr:
+    # TODO: Write your function code here
+
+main:  # DO NOT MODIFY THE MAIN SECTION
+	li $v0, 4
+	la $a0, cout
+	syscall
+
+	la $a0, array
+	li $a1, 10
+
+	jal printArr
+
+exit:
+	# TODO: Write code to properly exit a SPIM simulation
+
